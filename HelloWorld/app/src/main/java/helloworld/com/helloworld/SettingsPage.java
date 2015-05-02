681cd55228dbd24501046c8a54d2200414b50b9b
guttih <gudjonholm@gmail.com>
2015-05-02 23:18:09 +0000

    Opna heimasíðu úr settings

--- HelloWorld/app/src/main/java/helloworld/com/helloworld/SettingsPage.java
+++ HelloWorld/app/src/main/java/helloworld/com/helloworld/SettingsPage.java
@@ -1,9 +1,12 @@
 package helloworld.com.helloworld;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class SettingsPage extends ActionBarActivity {
@@ -35,5 +38,10 @@ public class SettingsPage extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void OpenWebPage(View view) {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(getResources().getString(R.string.url_website)));
+        startActivity(browserIntent);
     }
 }
